4770730ec33564326c720f1f0e451ba74832824b
Nguyen Dao Anh <nguyen.anh.dao@steadfasttech.com.au>
2015-09-11 10:38:38 +0700

    Add EF model

--- data-conversion/DataConversionTool/SQLScript/3.2.6.UpdateWorkbook_NotesTable.sql
+++
@@ -1,7 +0,0 @@
-﻿-- Update policies table
--- Document : https://steadfastgroup.sharepoint.com/teams/it/projects/boa/_layouts/15/WopiFrame2.aspx?sourcedoc={32549F3B-EC44-4E56-8D03-C4881AF2CE22}&file=BOA_Eclipse_Data_Conversion.docx&action=default
--- Section  : 3.2.6
-
-UPDATE dbo.Workbook_Notes SET wor_not_parent_id = pol_id
-
-FROM dbo.Workbook_Notes INNER JOIN policies ON wor_not_wor_id = pol_wor_id
--- data-conversion/DataConversionTool/SQLScript/4.5.AutoPolicyNumber.sql
+++
@@ -1,8 +0,0 @@
-﻿-- Auto policy number
--- Document : https://steadfastgroup.sharepoint.com/teams/it/projects/boa/_layouts/15/WopiFrame2.aspx?sourcedoc={32549F3B-EC44-4E56-8D03-C4881AF2CE22}&file=BOA_Eclipse_Data_Conversion.docx&action=default
--- Section  : 4.5
-
-insert into SystemConfigOptions (strOption, strOptionId)
-select	atconf_section, atconf_value
-from	atura_config
-where	atconf_section = 'auto_policy_number'
